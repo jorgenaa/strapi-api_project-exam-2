44b5ab1d60a6ea6f33c95a1ab642e959fffcbe2e
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2021-05-11 16:46:33 +0200

    changed the url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    url: env('https://strapi-api-holidaze.herokuapp.com/'),
+    url: env('https://strapi-api-holidaze.herokuapp.com'),
     admin: {
       auth: {
         secret: env('ADMIN_JWT_SECRET', '6d4335d3dc9ae6d6b9d5fd8e67492842'),
