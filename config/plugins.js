8609a9d9703c02e42878da471c5dcf5bd2f59c00
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2021-05-13 16:25:39 +0200

    modified the plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,6 @@ module.exports = ({ env }) => {
                 providerOptions: {
                     accessKeyId: env('AWS_ACCESS_KEY_ID'),
                     secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    accessKey: env('AWS_ACCESS_KEY'),
-                    secretKey: env('AWS_SECRET_KEY'),
                     region: env('AWS_REGION'),
                     params: {
                         Bucket: env('AWS_BUCKET')
